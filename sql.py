2049b82b59ddf0db4c0c61deb1c6bdf47ddb5d2d
Austin Heard <amheard0311@outlook.com>
2024-06-14 10:33:13 -0600

    Adds comments

--- sql.py
+++ sql.py
@@ -17,33 +17,41 @@ import psycopg2 as pg2
 import pandas as pd
 from datetime import datetime
 import os
-from dotenv import load_dotenv
+from dotenv import load_dotenv, error
 
 load_dotenv()
 
+# Establishes connection with database
 conn = pg2.connect(dbname=os.getenv('DBNAME'),
                    user=os.getenv('ADV_WORKS_USER'),
                    password = os.getenv('PASSWORD'),
                    host = os.getenv('HOST'),
                    port = os.getenv('PORT'))
 
+# Creates a var for reference to anything pulled from database
 cur = conn.cursor()
 
+# Given a sql file, an output path, and (optional) a name, queries the db and saves the results to the specified file path under the given name,
+#  Or under the name of the sql file given
 def convert_sql_to_xlsx(sql_in, xlsx_out, xlsx_name=None):
+
+    # Opens and reads the sql file
     sqlread = open(sql_in, 'r')
     sqlfile = sqlread.read()
     sqlread.close()
 
-    ##try:
-    cur.execute(sqlfile)
-    ##except OperationError,  msg:
-    ##print("Command")
+    # Executes the query from the sql in the database
+    try:
+        cur.execute(sqlfile)
+    except:
+        print(error)
+
+    
     df = pd.read_sql_query(sqlfile, conn)
+    if xlsx_name == None:
+        xlsx_name = sql_in[:-3]
+
     df.to_excel(f'{xlsx_out}/{xlsx_name}.xlsx', index=False)
-
-       
-
-
 #conn.commit()
 
 
